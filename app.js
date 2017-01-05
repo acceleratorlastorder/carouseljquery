16e6da82885c698bb4b331d6efe5b0feb92baf00
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-05 20:26:12 +0100

    update

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
         $('.imgcontainer .pic3').css({left: "200%"});
     });
     $('#buttoncontainer .2').click(function() {
-        $('.imgcontainer .pic1').css({left: "100%"});
-        $( ".imgcontainer .pic2" ).animate({ "left": "0%" }, "slow" );
-        $('.imgcontainer .pic3').css({left: "200%"});
+        $('.imgcontainer .pic1').animate({ "left": "-100%" }, "slow" );
+        $('.imgcontainer .pic2').animate({ "left": "0%" }, "slow" );
+        $('.imgcontainer .pic3').animate({ "left": "100%" }, "slow" );
 
     });
     $('#buttoncontainer .3').click(function() {
@@ -17,3 +17,6 @@ $(document).ready(function() {
         $('.imgcontainer .pic3').css({left: "0%"});
     });
 });
+/*
+faire un tableau d'une longueur x ( x étant le nombre de div ( idée faire un tableau ayant une taill dynamiquement crée afin qu'il soit de la taille du nombre de divs donc si 20 div alors le tableau aura 19 index)afin de permettre un réutilisation du code et d'en faire un plus robuste capable de marcher avec 3 images, 5000 ou plus et donc déplacer toute les images en même temps de par example - 200% ou -100%   bouger le 5 en première page donc -400% sur tout les éléments  idée de variable étant y = numéro de l'image +1*100 si elle est après alors - devant si avant alors + on aurait un genre de .animate({ "left": "y%" })
+*/
