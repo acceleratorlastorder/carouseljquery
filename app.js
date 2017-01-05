34ab438216cefbd090b15c71eeceece17aaa44fb
Cloud Strife <Cloud Strife>
2017-01-05 14:47:19 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 
     $('#buttoncontainer .1').click(function() {
-
-$('.imgcontainer');
+        $('.imgcontainer .pic1').css({left: "0%"});
+        $('.imgcontainer .pic2').css({left: "100%"});
+        $('.imgcontainer .pic3').css({left: "200%"});
+    });
+    $('#buttoncontainer .2').click(function() {
+        $('.imgcontainer .pic1').css({left: "100%"});
+        $('.imgcontainer .pic2').css({left: "0%"});
+        $('.imgcontainer .pic3').css({left: "200%"});
 
     });
+    $('#buttoncontainer .3').click(function() {
+        $('.imgcontainer .pic1').css({left: "200%"});
+        $('.imgcontainer .pic2').css({left: "100%"});
+        $('.imgcontainer .pic3').css({left: "0%"});
+    });
+
 });
